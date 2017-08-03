4ad47428a6e5b7a8e7d108c4061dd440e331e019
szberaz <szymon.zberaz@upaid.pl>
2017-08-03 20:11:06 +0200

    MC-0 Initialize repository

---
+++ src/main/java/pl/pragmaticcoders/interview/MarketCheckoutApplication.java
@@ -0,0 +1,12 @@
+package pl.pragmaticcoders.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketCheckoutApplication.class, args);
+	}
+}
